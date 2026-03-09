5297a621b8525e784c01bc6d62146a0a05f432a1
brendaBozac <159967145+brendaBozac@users.noreply.github.com>
2026-03-09 07:25:52 -0300

    feat: filter products by category using useParams

--- src/Containers/ItemListContainer/ItemListContainer.jsx
+++ src/Containers/ItemListContainer/ItemListContainer.jsx
@@ -1,20 +1,30 @@
 import { useState, useEffect } from "react"
-import { getProducts } from "../../Data/asyncMock"
+import { getProducts, getProductsByCategory } from "../../Data/asyncMock"
 import ItemList from "../../Components/ItemList/ItemList"
+import { useParams } from "react-router-dom"
 
 
 const ItemListContainer = () => {
 
   const [products, setProducts] = useState([])
 
+  const { categoryId } = useParams()
+
   useEffect(() => {
 
-    getProducts()
-      .then((res) => {
-        setProducts(res)
-      })
+    if (categoryId) {
 
-  }, [])
+      getProductsByCategory(categoryId)
+        .then(res => setProducts(res))
+
+    } else {
+
+      getProducts()
+        .then(res => setProducts(res))
+
+    }
+
+  }, [categoryId])
 
   return (
     <div>
