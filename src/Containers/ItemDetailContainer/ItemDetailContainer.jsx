2ac7b5dd2da9ce2125341dadaf3efe286d3ed794
brendaBozac <159967145+brendaBozac@users.noreply.github.com>
2026-03-10 08:34:23 -0300

    feat: fetch product data in ItemDetailContainer using asyncMock

--- src/Containers/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/Containers/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,14 +1,43 @@
+import { useState, useEffect } from "react"
 import { useParams } from "react-router-dom"
+import { getProductById } from "../../Data/asyncMock"
 
 const ItemDetailContainer = () => {
 
   const { productId } = useParams()
 
-  console.log(productId)
+  const [product, setProduct] = useState(null)
+
+  useEffect(() => {
+
+    getProductById(productId)
+      .then(response => {
+        setProduct(response)
+      })
+
+  }, [productId])
+
+
+  if (!product) {
+    return <h2>Loading product...</h2>
+  }
 
   return (
-    <h2>Detalle del producto {productId}</h2>
+
+    <div>
+
+      <h2>{product.title}</h2>
+
+      <img src={product.image} alt={product.title} />
+
+      <p>${product.price}</p>
+
+      <p>{product.description}</p>
+
+    </div>
+
   )
+
 }
 
 export default ItemDetailContainer
