cc3213b6c53dcdcad5ca7fcbdd35742342600227
brendaBozac <159967145+brendaBozac@users.noreply.github.com>
2026-03-17 07:41:53 -0300

    feat: improve item detail UX with conditional itemcount and cart quantity indicator

--- src/Components/ItemDetail/ItemDetail.jsx
+++ src/Components/ItemDetail/ItemDetail.jsx
@@ -1,15 +1,23 @@
 import styles from "./ItemDetail.module.css"
 import ItemCount from "../ItemCount/ItemCount"
 
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { CartContext } from "../../Context/CartContext"
+import { Link } from "react-router-dom"
 
 const ItemDetail = ({ product }) => {
 
-    const { addItem } = useContext(CartContext)
+    const { cart, addItem } = useContext(CartContext)
+
+    const itemInCart = cart.find(item => item.id === product.id)
+
+    const [quantityAdded, setQuantityAdded] = useState(0)
+
+    if (!product) return <p>Cargando producto...</p>
 
     const handleAdd = (quantity) => {
         addItem(product, quantity)
+        setQuantityAdded(quantity)
     }
 
     return (
@@ -30,15 +38,39 @@ const ItemDetail = ({ product }) => {
                 {product.description}
             </p>
 
-            <ItemCount onAdd={handleAdd} />
+            {itemInCart && (
+                <p>Ya tienes {itemInCart.quantity} en el carrito</p>
+            )}
+
+            {quantityAdded > 0 ? (
+
+                <div>
+
+                    <p>Producto agregado al carrito</p>
+
+                    <Link to="/cart">
+                        <button>Ir al carrito</button>
+                    </Link>
+
+                    <Link to="/">
+                        <button>Seguir comprando</button>
+                    </Link>
+
+                </div>
+
+            ) : (
+
+                <ItemCount
+                    initial={1}
+                    stock={10}
+                    onAdd={handleAdd}
+                />
+
+            )}
 
         </article>
 
     )
-
 }
-
-
-
 
 export default ItemDetail
