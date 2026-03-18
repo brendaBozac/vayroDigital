ca8c6dfacb722200f3aeee451023f3d6e1988ab6
brendaBozac <159967145+brendaBozac@users.noreply.github.com>
2026-03-18 06:54:05 -0300

    chore: install firebase and add initial firebase configuration

---
+++ src/Services/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCgg5rMbbfvE8WXSn4BVcED_m96tNjs3f4",
+  authDomain: "vayro-backend.firebaseapp.com",
+  projectId: "vayro-backend",
+  storageBucket: "vayro-backend.firebasestorage.app",
+  messagingSenderId: "305942201628",
+  appId: "1:305942201628:web:740b3252c737b6be97a6d9"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
